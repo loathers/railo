02369d1076f038e8c6b9c6d664ea62cc28a1edf0
horrible little slime <69secret69email69@gmail.com>
2022-10-11 17:43:35 -0400

    Don't explode on cleaver beaten up

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,8 @@ import {
   Macro,
   Session,
   sinceKolmafiaRevision,
+  uneffect,
+  withProperty,
 } from "libram";
 
 import { freeFightFamiliar } from "./familiar";
@@ -123,7 +125,13 @@ export function main(command?: string) {
       {
         name: "June Cleaver",
         completed: () => !!get("_juneCleaverFightsLeft"),
-        do: $location`Noob Cave`,
+        do: () =>
+          withProperty("recoveryScript", "", () => {
+            adv1($location`Noob Cave`, -1, "");
+            if (["Poetic Justice", "Lost and Found"].includes(get("lastEncounter"))) {
+              uneffect($effect`Beaten Up`);
+            }
+          }),
         choices: Object.fromEntries(
           JuneCleaver.choices.map((choice) => [
             choice,
@@ -133,6 +141,7 @@ export function main(command?: string) {
         sobriety: "either",
         ready: () => JuneCleaver.have() && !get("_juneCleaverFightsLeft"),
         outfit: { weapon: $item`June cleaver` },
+        combat: new CombatStrategy().macro(Macro.abort()),
       },
       {
         name: "Proton Ghost",
